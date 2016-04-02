486f860d8bbb8e4485021fa8b5d5fab6fecad704
Joseph Luck <joseph.luck@sky.com>
2016-04-02 13:59:45 +0100

    Allowing for multi-page request and hiding on page load, showing when data has arrived

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 import Handlebars from 'libs/handlebars';
 
-
 function getPageData(source) {
   let page_id = source.dataset.airPage,
       api_url = window.API_ROOT + 'pages/' + page_id;
@@ -25,23 +24,23 @@ function getPageData(source) {
   });
 }
 
-function prepareAndRenderPage(source, context) {
+function renderPage(source, context) {
   var template = Handlebars.compile(source.innerHTML),
       html = template(context);
 
-  renderPage(source, html);
-}
-
-function renderPage(source, html) {
   source.innerHTML = html;
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  let source = document.getElementById("entry-template");
+  let sources = document.querySelectorAll("#entry-template");
 
-  getPageData(source).then(function(resp) {
-    prepareAndRenderPage(source, resp);
-  }, function() {
-    debugger
-  })
+  for (let i = 0, x = sources.length; i < x; i++) {
+    let source = sources[i];
+    getPageData(source).then(function(resp) {
+      renderPage(source, resp);
+      source.style.visibility = "visible";
+    }, function() {
+      console.warn('Show an API error here');
+    })
+  }
 });
